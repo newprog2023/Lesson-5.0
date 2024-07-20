ab3964a9ae018139be22bd90a369a1259e13b81b
newprog2023 <newprog@internet.ru>
2024-07-20 17:53:18 +0300

    two bank account

--- main.py
+++ main.py
@@ -1,53 +1,49 @@
-class Warrior():
-    def __init__(self, name, power, endurance, hair_color):
-        self.name = name
-        self.power = power
-        self.endurance = endurance
-        self.hair_color = hair_color
+class Account():
+    def __init__(self, client_id, count):
+        self.client_id = client_id
+        self.count = count
 
-    def sleep(self):
-        print(f'{self.name} лег спать')
-        self.endurance += 2
+    def deposit(self):
+        dep = int(input ('сколько внесём рублей ?'))
+        print(f'{self.client_id} пополняет счёт на {dep} руб.')
+        self.count += dep
 
-    def eat(self):
-        print(f'{self.name} сел кушать')
-        self.power += 1
-
-    def hit(self):
-        print(f'{self.name} бьет кого-то')
-        self.endurance -= 6
-        self.power -= 2
-
-    def walk(self):
-        print(f'{self.name} гуляет')
-        self.endurance -= 1
-        self.power += 1
+    def snimaem(self):
+        dep = int(input ('сколько снимем рублей ?'))
+        if dep > self.count:
+            print(f"На счете всего {self.count} нельзя снять больше")
+        else:
+            print(f'{self.client_id} снимает со счёта {dep} руб.')
+            self.count -= dep
 
     def info(self):
-        print(f'имя воина -          {self.name}')
-        print(f"цвет волос воина -   {self.hair_color}")
-        print(f"сила воина -         {self.power}")
-        print(f'выносливость воина - {self.endurance}')
+        print(f'имя клиента - {self.client_id}')
+        print(f"на счету -    {self.count}")
 
+war1 = Account("Илья", 76)
+war2 = Account("Добрыня", 60)
 
-war1 = Warrior("Илья", 76, 54,"блондин")
-war2 = Warrior("Добрыня", 60, 60,"рыжий")
-
-war1.info()
-war2.info()
-
-war1.sleep()
-war2.eat()
-war1.hit()
-war2.sleep()
-war1.eat()
-war2.hit()
-war1.walk()
-war2.walk()
-war2.eat()
-war1.eat()
-war2.hit()
-
-war1.info()
-war2.info()
-
+while True:
+    name = int(input('Ваше имя Илья (1) или Добрыня (2) ?'))
+    if name != 1 and name != 2:
+        print('недопустимый ввод. Попробуйте ответить еще раз, введите 1 или 2')
+        continue
+    do = int(input('Желаете пополнить счет (1) или снять деньги (2) ?'))
+    if do != 1 and do != 2:
+        print('недопустимый ввод. Попробуйте ответить еще раз, введите 1 или 2')
+        continue
+#    money = int(input('Введите сумму в рублях ?'))
+    if name == 1:
+        war1.info()
+        if do == 1:
+            war1.deposit()
+        elif do == 2:
+            war1.snimaem()
+        war1.info()
+    elif name == 2:
+        war2.info()
+        if do == 1:
+            war2.deposit()
+        elif do == 2:
+            war2.snimaem()
+        war2.info()
