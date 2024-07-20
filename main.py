4d62618ca5ddec1a8de69035445834d8d11ca9a0
newprog2023 <newprog@internet.ru>
2024-07-20 16:39:08 +0300

    create objects 1 and 2

--- main.py
+++ main.py
@@ -1,30 +1,53 @@
-class Warrior ():
-    def init (self, name, power, endurance, hair_color):
+class Warrior():
+    def __init__(self, name, power, endurance, hair_color):
         self.name = name
         self.power = power
         self.endurance = endurance
         self.hair_color = hair_color
 
-    def sleep (self):
+    def sleep(self):
         print(f'{self.name} лег спать')
-        self.endurance =+ 2
+        self.endurance += 2
 
-    def eat (self):
+    def eat(self):
         print(f'{self.name} сел кушать')
-        self.power =+ 1
+        self.power += 1
 
-    def hit (self):
+    def hit(self):
         print(f'{self.name} бьет кого-то')
-        self.endurance = - 6
-        self.power = - 2
-
-    def walk (self):
-        print(f'{self.name} гуляет')
-        self.endurance = - 1
-        self.power = + 1
+        self.endurance -= 6
+        self.power -= 2
 
     def walk(self):
-        print(f'имя воина - {self.name}')
-        print(f"цвет волос воина - {self.hair_color}")
-        print(f"сила воина - {self.power}")
+        print(f'{self.name} гуляет')
+        self.endurance -= 1
+        self.power += 1
+
+    def info(self):
+        print(f'имя воина -          {self.name}')
+        print(f"цвет волос воина -   {self.hair_color}")
+        print(f"сила воина -         {self.power}")
         print(f'выносливость воина - {self.endurance}')
+
+
+war1 = Warrior("Илья", 76, 54,"блондин")
+war2 = Warrior("Добрыня", 60, 60,"рыжий")
+
+war1.info()
+war2.info()
+
+war1.sleep()
+war2.eat()
+war1.hit()
+war2.sleep()
+war1.eat()
+war2.hit()
+war1.walk()
+war2.walk()
+war2.eat()
+war1.eat()
+war2.hit()
+
+war1.info()
+war2.info()
+
