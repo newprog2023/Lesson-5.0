635e3d94718140bcc8481f4234706454c4c29db1
newprog2023 <newprog@internet.ru>
2024-07-20 16:01:46 +0300

    add class Warrior

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 class Warrior ():
-    def init(self, name, power, endurance, hair_color):
+    def init (self, name, power, endurance, hair_color):
         self.name = name
         self.power = power
         self.endurance = endurance
         self.hair_color = hair_color
 
-    def sleep ():
+    def sleep (self):
         print(f'{self.name} лег спать')
         self.endurance =+ 2
 
-    def eat(self):
+    def eat (self):
         print(f'{self.name} сел кушать')
         self.power =+ 1
 
-    def hit(self):
+    def hit (self):
         print(f'{self.name} бьет кого-то')
-        self.endurance =- 6
+        self.endurance = - 6
+        self.power = - 2
+
+    def walk (self):
+        print(f'{self.name} гуляет')
+        self.endurance = - 1
+        self.power = + 1
 
     def walk(self):
-        print(f'{self.name} гуляет')
-
-def walk(self):
-print(f”имя воина - {self.name}”)
-print(f”цвет волос воина - {self.hair_color}”)
-print(f”сила воина - {self.power}”)
-print(f”выносливость воина - {self.endurance}”)
+        print(f'имя воина - {self.name}')
+        print(f"цвет волос воина - {self.hair_color}")
+        print(f"сила воина - {self.power}")
+        print(f'выносливость воина - {self.endurance}')
